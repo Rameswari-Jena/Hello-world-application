b0bf5c500e132c2051b81c7b173115c1b1e5c9d0
Rameswari-Jena <reach2rameswarijena@gmail.com>
2023-03-17 18:10:43 +0800

    commited all 3 folders

---
+++ Terraform-child-modules/VPC/main.tf
@@ -0,0 +1,17 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = var.vpc_name
+  cidr = var.cidr
+
+  azs             = var.availability_zones
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+  
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
