cf93e54098393bd3e14e02a0066935072246e7fc
Rameswari-Jena <reach2rameswarijena@gmail.com>
2023-03-26 11:19:12 +0530

    modified the iam role

--- child-modules/IAM-for-lambda/main.tf
+++ child-modules/IAM-for-lambda/main.tf
@@ -35,7 +35,7 @@ resource "aws_iam_role" "lambda-role" {
   "Version": "2012-10-17",
   "Statement": {
   "Effect": "Allow",
-  "Principal": {"Service": "ec2.amazonaws.com"},
+  "Principal": {"Service": "lambda.amazonaws.com"},
   "Action": "sts:AssumeRole"
   }
   }
