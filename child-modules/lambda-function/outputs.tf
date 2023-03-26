7e71c2004f8e857a2330aff00e5fc15aa285f9cd
Rameswari-Jena <reach2rameswarijena@gmail.com>
2023-03-26 18:44:41 +0530

    update output file

--- child-modules/lambda-function/outputs.tf
+++ child-modules/lambda-function/outputs.tf
@@ -1,15 +1,23 @@
 output "lambda_function_1_name" {
-  value = aws_lambda_function.hello_world_lambda_stop_ec2.qualified_arn
+  value = aws_lambda_function.hello_world_lambda_stop_ec2.function_name
 }
 
 output "lambda_function_1_id" {
   value = aws_lambda_function.hello_world_lambda_stop_ec2.id
 }
 
+output "lambda_function_1_arn" {
+  value = aws_lambda_function.hello_world_lambda_stop_ec2.qualified_arn
+}
+
 output "lambda_function_2_name" {
-  value = aws_lambda_function.hello_world_lambda_start_ec2.qualified_arn
+  value = aws_lambda_function.hello_world_lambda_start_ec2.function_name
 }
 
 output "lambda_function_2_id" {
   value = aws_lambda_function.hello_world_lambda_start_ec2.id
+}
+
+output "lambda_function_2_arn" {
+  value = aws_lambda_function.hello_world_lambda_start_ec2.qualified_arn
 }
